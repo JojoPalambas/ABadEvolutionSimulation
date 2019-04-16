7a725365c40fdd884869a9ac29a7904e24e26fbc
Gabriel Depotte <depott_g@epita.fr>
2019-04-16 15:25:08 +0200

    [general] Finished a first version of the ProgramManager and CameraManager

---
+++ Assets/Scripts/Singletons/CameraManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Vector3 targetPosition;
+    public Vector3 targetRotation;
+    [Range(0, 1)]
+    public float bias;
+    //public float rotationSpeed;
+
+    public static CameraManager instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetPosition != null)
+        {
+            transform.position = transform.position * bias + targetPosition * (1 - bias);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(targetRotation), bias);
+        }
+    }
+}
---
+++ Assets/Scripts/Singletons/ProgramManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgramManager : MonoBehaviour
+{
+    public enum Status
+    {
+        main_menu,
+        survival
+    }
+
+    public static ProgramManager instance;
+
+    private Status status;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+
+        MainMenu();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void MainMenu()
+    {
+        status = Status.main_menu;
+    }
+
+    public void Survival()
+    {
+        status = Status.survival;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}

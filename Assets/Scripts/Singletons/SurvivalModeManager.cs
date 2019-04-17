71b4fdd525cdbabc3990bac2cac87098dbbc22cc
Gabriel Depotte <depott_g@epita.fr>
2019-04-17 11:24:00 +0200

    [map] Added a palette, tiles, and added a first map to the game

---
+++ Assets/Scripts/Singletons/SurvivalModeManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SurvivalModeManager : MonoBehaviour
+{
+    public Grid grid;
+    public Tilemap tilemap; 
+
+    public static SurvivalModeManager instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Remove()
+    {
+        instance = null;
+        Destroy(gameObject);
+    }
+}
